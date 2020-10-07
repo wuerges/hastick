65de4130729f57d07172159bff494cb2a9377e18
Emilio Wuerges <wuerges@gmail.com>
2020-10-06 21:11:21 -0300

    starting parser

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, parsec, stdenv }:
 mkDerivation {
   pname = "hastick";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base ];
+  libraryHaskellDepends = [ base parsec ];
   executableHaskellDepends = [ base ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
